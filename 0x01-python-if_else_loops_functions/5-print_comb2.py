2bbbcff25619bd8dfe741ad0f6a4dee75f3bac05
lydiauzoma <99988048+lydiauzoma@users.noreply.github.com>
2022-09-21 00:28:26 +0100

    hot fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
- 
-for num range(0, 100):
-	if num == 99:
-	print(num)
-else:
-	print('{:02d},'.format(num), end=" ")
+
+for num in range(0, 100):
+    if num == 99:
+        print(num)
+    else:
+        print('{:02d},'.format(num), end=" ")
