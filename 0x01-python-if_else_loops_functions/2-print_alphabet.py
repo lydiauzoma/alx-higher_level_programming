376cb156e13f2202e5b9dbdf810a7a7072c4d3d3
lydiauzoma <99988048+lydiauzoma@users.noreply.github.com>
2022-09-21 01:04:59 +0100

    hot fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-print the ASCII alphabet in lowercase, not followed by a new line.
+Print the ASCII alphabet in lowercase, not followed by a new line.
 """
 for letter in range(97, 123):
-	print("{}".format(chr(letter)), end="")
+    print("{}".format(chr(letter)), end="")
