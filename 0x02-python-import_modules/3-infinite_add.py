2dbe1ad799f1afa81a5fd3d8d504f1bdc37ab289
lydiauzoma <99988048+lydiauzoma@users.noreply.github.com>
2022-09-23 02:11:06 +0100

    hot fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
-#!/usr?python3
+#!/usr/bin/python3
 if __name__ == "__main__":
- from sys import argv
- varLen = len(argv) - 1
+    from sys import argv
+    varLen = len(argv) - 1
 
- result = 0
- for i in range(1, varLen + 1):
- result += int(argv[i])
- print(result)
+    result = 0
+    for i in range(1, varLen + 1):
+        result += int(argv[i])
+    print(result)
