8833ca685a09dfb4c9ca1949dbe535f87d596c06
lydiauzoma <99988048+lydiauzoma@users.noreply.github.com>
2022-09-23 02:10:07 +0100

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python#
+#!/usr/bin/python3
 if __name__ == "__main__":
- from sys import argv
- lenofArg = (len(argv)
+    from sys import argv
+    lenOfArg = len(argv)
 
- if lenArg == 1:
-  print("0 arguments.")
- elif lenofArg == 2:
-  print("{} arguments:".format(lenofArg - 1)
- else:
-  print("{} argguments:".format(lenofArg - 1)
- for index, var in enumerate(argv):
-  if index == 0:
- continue
-  print("{}: {}".format(index, var))
+    if lenOfArg == 1:
+        print("0 arguments.")
+    elif lenOfArg == 2:
+        print("{} argument:".format(lenOfArg - 1))
+    else:
+        print("{} arguments:".format(lenOfArg - 1))
+    for index, var in enumerate(argv):
+        if index == 0:
+            continue
+        print("{}: {}".format(index, var))
