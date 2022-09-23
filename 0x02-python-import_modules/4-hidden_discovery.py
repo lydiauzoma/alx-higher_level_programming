adc39f9aa17b4fb2dd6f15139cf5fefb00215938
lydiauzoma <99988048+lydiauzoma@users.noreply.github.com>
2022-09-23 19:48:16 +0100

    hot fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-
-    name = dir(hidden_4)
-    for names in name:
-        if names[:2] != "__":
-            print(names)
+    for s in dir(hidden_4):
+        if s[:2] != "__":
+            print("{:s}".format(s))
