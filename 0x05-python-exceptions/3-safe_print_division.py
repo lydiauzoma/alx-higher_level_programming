47dee823348194af5e57cfd8375fd70697b6e530
lydia uzoma <99988048+lydiauzoma@users.noreply.github.com>
2022-10-10 15:14:56 +0100

    hot fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    counter = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-        except (ValueError, TypeError):
-            pass
-        else:
-            counter += 1
-    print()
-    return (counter)
+def safe_print_division(a, b):
+    try:
+        result = a/b
+    except:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return (result)
